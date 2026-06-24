9f16ef1acc7b24f910eec17a01936fb993cea8f7
Babuchittis <chitti.babu.s@sap.com>
2026-06-24 10:47:41 +0000

    commit2

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
+// const taskManager = require("./taskManager");
+
+// const tasks = taskManager.getTasks();
+
+// console.log("Tasks from Custom Module:");
+// console.log(tasks);
+
+
+
+
+
+// const taskManager = require("./taskManager");
+
+// taskManager
+//     .getTasksPromise()
+//     .then(tasks => {
+//         console.log("Tasks using Promise:");
+//         console.log(tasks);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+
+
+
+
+const express = require("express");
+const dayjs = require("dayjs");
 const taskManager = require("./taskManager");
 
-const tasks = taskManager.getTasks();
+const app = express();
+const PORT = 8080;
 
-console.log("Tasks from Custom Module:");
-console.log(tasks);
+// Default Route
+app.get("/", (req, res) => {
+    const currentDate = dayjs().format("DD-MM-YYYY HH:mm:ss");
+
+    res.send(`Current Date & Time: ${currentDate}`);
+});
+
+// Tasks Route
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await taskManager.getTasksPromise();
+        res.json(tasks);
+    } catch (error) {
+        res.status(500).send("Error loading tasks");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- taskManager.js
+++ taskManager.js
@@ -1,26 +1,75 @@
+// const fs = require("fs");
+// const path = require("path");
+
+// // Global Object (__dirname)
+// const filePath = path.join(__dirname, "data.json");
+
+// // Read and display file content
+// fs.readFile(filePath, "utf8", (err, data) => {
+//     if (err) {
+//         console.log("Error reading file:", err);
+//         return;
+//     }
+
+//     console.log("Data from JSON:");
+//     console.log(JSON.parse(data));
+// });
+
+// // Custom Module Function
+// function getTasks() {
+//     const data = fs.readFileSync(filePath, "utf8");
+//     return JSON.parse(data);
+// }
+
+// module.exports = {
+//     getTasks
+// };
+
+// const fs = require("fs");
+// const path = require("path");
+
+// const filePath = path.join(__dirname, "data.json");
+
+// // Promise Function
+// function getTasksPromise() {
+//     return new Promise((resolve, reject) => {
+//         fs.readFile(filePath, "utf8", (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(JSON.parse(data));
+//             }
+//         });
+//     });
+// }
+
+// module.exports = {
+//     getTasksPromise
+// };
+
+
+
+
+
+
+
 const fs = require("fs");
 const path = require("path");
 
-// Global Object (__dirname)
 const filePath = path.join(__dirname, "data.json");
 
-// Read and display file content
-fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-        console.log("Error reading file:", err);
-        return;
-    }
-
-    console.log("Data from JSON:");
-    console.log(JSON.parse(data));
-});
-
-// Custom Module Function
-function getTasks() {
-    const data = fs.readFileSync(filePath, "utf8");
-    return JSON.parse(data);
+function getTasksPromise() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(data));
+            }
+        });
+    });
 }
 
 module.exports = {
-    getTasks
+    getTasksPromise
 };
